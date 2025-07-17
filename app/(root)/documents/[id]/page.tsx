b6f818a000f7f2f9f703ce70852222c97192eb29
Sara-ibrahiim <saraibrahim.0260@gmail.com>
2025-07-17 19:29:52 +0300

    auth-clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -14,7 +14,7 @@ const Document = () => {
     <div>
       <Header>
         <div className="flex w-fit items-center justify-center gap-2">
-          <p className="document-title"></p>
+          <p className="document-title">Share</p>
         </div>
 
         <SignedOut>

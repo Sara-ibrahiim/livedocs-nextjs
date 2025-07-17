736e0ea18b4e4a6da25de94d70b9dfff4d6b9b0e
Sara-ibrahiim <saraibrahim.0260@gmail.com>
2025-07-17 18:10:00 +0300

    header

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,12 +1,18 @@
-import { Editor } from '@/components/editor/Editor'
-import React from 'react'
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import React from "react";
 
 const Document = () => {
   return (
     <div>
-    <Editor/>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title"></p>
+        </div>
+      </Header>
+      <Editor />
     </div>
-  )
-}
+  );
+};
 
-export default Document
+export default Document;

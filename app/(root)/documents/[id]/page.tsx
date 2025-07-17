48c943b82bb6e23efce6dfdb0e76a79542c62367
Sara-ibrahiim <saraibrahim.0260@gmail.com>
2025-07-17 19:22:48 +0300

    auth-clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,12 @@
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import React from "react";
 
 const Document = () => {
@@ -9,6 +16,14 @@ const Document = () => {
         <div className="flex w-fit items-center justify-center gap-2">
           <p className="document-title"></p>
         </div>
+
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton></SignUpButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
       <Editor />
     </div>

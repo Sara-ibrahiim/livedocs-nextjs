69813f74ae0859522b15d3034f6904931ada6cb5
Sara-ibrahiim <saraibrahim.0260@gmail.com>
2025-07-27 18:56:00 +0300

    CollaborativeRoom

--- components/AddDocumentBtn.tsx
+++ components/AddDocumentBtn.tsx
@@ -1,21 +1,28 @@
-"use client"
+"use client";
+import { createDocument } from "@/lib/actions/room.actions";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 
 const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
-  const addDocumentHandller = async () => {};
- 
+  const router = useRouter();
+  const addDocumentHandller = async () => {
+    try {
+      const room = await createDocument({ userId, email });
+      if (room) router.push(`/documents/${room.id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Button
       className="gradient-blue flex gap-1 shadow-md"
       onClick={addDocumentHandller}
       type="submit"
     >
-        <Image 
-        src="/assets/icons/add.svg"
-        alt="add"
-        width={24}
-        height={24}/>
+      <Image src="/assets/icons/add.svg" alt="add" width={24} height={24} />
       <p className="hidden sm:block"> Start a blank document </p>
     </Button>
   );
